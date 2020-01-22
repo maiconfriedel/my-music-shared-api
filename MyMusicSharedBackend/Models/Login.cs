708803d54697088b33bf0417b8fb5d4d7a8772a9
Maicon Gabriel Friedel <maicon.friedel@kyly.com.br>
2020-01-22 17:20:36 -0300

    Added Required Data Annotations

--- MyMusicSharedBackend/Models/Login.cs
+++ MyMusicSharedBackend/Models/Login.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,16 +14,19 @@ namespace MyMusicSharedBackend.Models
         /// <summary>
         /// User name
         /// </summary>
+        [Required]
         public string Username { get; set; }
 
         /// <summary>
         /// Password of the user
         /// </summary>
+        [Required]
         public string Password { get; set; }
 
         /// <summary>
         /// Scopes to authenticate
         /// </summary>
+        [Required]
         public IEnumerable<string> Scopes { get; set; }
 
         /// <summary>
